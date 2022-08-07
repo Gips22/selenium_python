c454aff0676714de90996de7addbbe49dbdac18e
Andrey Naletov <gipsme123@gmail.com>
2022-08-07 17:22:56 +0530

    Что сделал: 1. Сделал подключение к БД в основном файле программы. 2.Сделал автоматическое создание папки для скриншотов и создание самих скринов лотов 3. Сделал рабочую выгрузку в БД всех данных кроме скриншотов

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -2,14 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service # чтобы не вылезала ошибка "Executable path has been deprecated please pass in a Service object"
 from fake_useragent import UserAgent
 import time
-useragent = UserAgent(verify_ssl=False)
 
+useragent = UserAgent(verify_ssl=False)
 
 s = Service("/Users/andreynaletov/Desktop/PROJECTS/Projects/selenium_python/chromedriver/chromedriver") # создаем переменную, чтобы не вылезала ошибка Executable path has been deprecated please pass in a Service object.
 options = webdriver.ChromeOptions()
-# options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
-# options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument(f"user-agent={useragent.random}") # verify_ssl=False помогла настроить работу пакета fake-useragent и зафиксила ошибку
+
+# set proxy
+# добавляем новый аргумент в объект опций
+# options.add_argument("--proxy-server=138.128.91.65:8000")
+
 
 url = "https://www.avito.ru/"
 driver = webdriver.Chrome(service=s, options=options) # здесь в service удаляем прямой путь к драйверу и указываем переменную s
